d8a55ccfb1c49878ad2e71caa5aae75e504581b6
ccs100203 <ccs100203@gmail.com>
2022-05-22 13:21:03 +0800

    Add memory allocation & release
    Pass address sanitizer
    Fail thread sanitizer on `free()`, I will confirm again.

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 #include <stdatomic.h>
 #include "lib.h"
 
-#define ver_num 5
-uint64_t data[ver_num+1] = {10, 11, 12, 13, 14, 15}; // lock? atomic?
-// uint64_t version = 0;
+#define NUM_VER 5
+uint64_t *data[NUM_VER+1];
 atomic_uint_fast64_t version = 0;
 
 // #define OUTPUT_TO_BUF 1
@@ -17,10 +16,9 @@ char output_len = 0;
 void *writer(void *arg)
 {
     struct qsbr *q = (struct qsbr *) arg;
-    for (int i = 0; i < ver_num; ++i) {
-        sleep(4);
-        atomic_fetch_add_explicit(&version, 1, memory_order_relaxed); 
-        // version++;
+    for (int i = 0; i < NUM_VER; ++i) {
+        sleep(2);
+        atomic_fetch_add_explicit(&version, 1, memory_order_relaxed);
         
     #ifdef OUTPUT_TO_BUF
         pthread_mutex_lock(&mu);
@@ -28,11 +26,11 @@ void *writer(void *arg)
         output_len = strlen(output_buf);
         pthread_mutex_unlock(&mu);
     #else
-        printf("\n---- begin of grace period: %lu -----\n", version);
+        printf("\n---- begin of grace period: %lu -----\n", atomic_load_explicit(&version, memory_order_relaxed));
         fflush(stdout);
     #endif
 
-        qsbr_wait(q, version);
+        qsbr_wait(q, atomic_load_explicit(&version, memory_order_relaxed));
         
     #ifdef OUTPUT_TO_BUF
         pthread_mutex_lock(&mu);
@@ -40,11 +38,12 @@ void *writer(void *arg)
         output_len = strlen(output_buf);
         pthread_mutex_unlock(&mu);
     #else
-        printf("---- end of grace period: %lu ------\n\n", version);
+        printf("---- end of grace period: %lu ------\n\n", atomic_load_explicit(&version, memory_order_relaxed));
         fflush(stdout);
     #endif
-        
-        // reclamation memory, TODO
+        // reclamation memory
+        free(data[atomic_load_explicit(&version, memory_order_acquire)-1]);
+        fflush(stdout);
     }
     pthread_exit(NULL);
 }
@@ -56,14 +55,11 @@ void *reader(void *arg)
     while (1) {
         uint64_t v = atomic_load_explicit(&version, memory_order_relaxed);
         // uint64_t v = version;
-        uint64_t tmp = data[v]; // access data
-        printf("tid: %d access  data, %lu\n", gettid(), tmp);
-        fflush(stdout);
+        uint64_t tmp = *data[v]; // access data
+        // printf("tid: %d access data, %lu\n", gettid(), tmp);
+        // fflush(stdout);
         sleep(rand() % 3 + 2); // hold data and do nothing
         
-        // qsbr_update(ref, v); // on quiescent
-        qsbr_update(ref, atomic_load_explicit(&version, memory_order_relaxed)); // on quiescent
-
     #ifdef OUTPUT_TO_BUF
         pthread_mutex_lock(&mu);
         sprintf(output_buf+output_len, "tid: %d release data, %lu\n", gettid(), tmp);
@@ -73,8 +69,10 @@ void *reader(void *arg)
         printf("tid: %d release data, %lu\n", gettid(), tmp);
         fflush(stdout);
     #endif
-        
-        if(v == ver_num) {
+
+        qsbr_update(ref, atomic_load_explicit(&version, memory_order_relaxed)); // on quiescent
+
+        if(v == NUM_VER) {
             break;
         }
 
@@ -89,17 +87,17 @@ void *reader2(void *arg)
 
     while (1) {
         uint64_t v = atomic_load_explicit(&version, memory_order_relaxed);
-        uint64_t tmp = data[v]; // access data
-        printf("tid: %d access  data, %lu\n", gettid(), tmp);
-        fflush(stdout);
+        uint64_t tmp = *data[v]; // access data
+        // printf("tid: %d access data, %lu\n", gettid(), tmp);
+        // fflush(stdout);
         sleep(rand() % 1 + 1); // hold data and do nothing
         
-        // qsbr_update(ref, v); // on quiescent
-        qsbr_update(ref, atomic_load_explicit(&version, memory_order_relaxed)); // on quiescent
         printf("tid: %d release data, %lu\n", gettid(), tmp);
         fflush(stdout);
+        qsbr_update(ref, atomic_load_explicit(&version, memory_order_relaxed)); // on quiescent
+        
 
-        if(v == ver_num) {
+        if(v == NUM_VER) {
             break;
         }
 
@@ -109,31 +107,43 @@ void *reader2(void *arg)
     pthread_exit(NULL);
 }
 
+#define NUM_READER 5
+#define NUM_TID 1 + NUM_READER
+
 int main()
 {
+    for(int i=0; i<=NUM_VER; ++i) {
+        data[i] = malloc(sizeof(uint64_t));
+        *data[i] = 20 + i;
+    }
+
     srand(time(NULL));
     struct qsbr *q = qsbr_create();
-    struct qsbr_ref qref, qref2, qref3;
-    qsbr_register(q, &qref);
-    qsbr_register(q, &qref2);
-    qsbr_register(q, &qref3);
-#define tid_n 1 + 3
-    pthread_t tid[tid_n];
+
+
+    struct qsbr_ref qrefs[NUM_READER];
+    for (int i=0; i<NUM_READER; ++i) {
+        qsbr_register(q, &qrefs[i]);
+    }
+
+    pthread_t tid[NUM_TID];
     pthread_create(tid, NULL, writer, q);
-    pthread_create(tid + 1, NULL, reader, &qref);
-    pthread_create(tid + 2, NULL, reader, &qref2);
-    pthread_create(tid + 3, NULL, reader2, &qref3);
-    for (int i = 0; i < tid_n; ++i) {
+    for (int i=0; i<NUM_READER; ++i) {
+        if(i%2 == 0) {
+            pthread_create(tid + 1 + i, NULL, reader, &qrefs[i]);
+        } else {
+            pthread_create(tid + 1 + i, NULL, reader2, &qrefs[i]);
+        }
+    }
+
+    for (int i = 0; i < NUM_TID; ++i) {
         pthread_join(tid[i], NULL);
-        // if (pthread_join(tid[i], NULL) == 0) {
-        //     printf("join\n");
-        // }
     }
 
     printf("After join\n");
-    qsbr_unregister(q, &qref);
-    qsbr_unregister(q, &qref2);
-    qsbr_unregister(q, &qref3);
+    for (int i=0; i<NUM_READER; ++i) {
+        qsbr_unregister(q, &qrefs[i]);
+    }
     qsbr_destroy(q);
 #ifdef OUTPUT_TO_BUF
     printf("%s", output_buf);
